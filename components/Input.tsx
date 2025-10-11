dafc31a898ce6c17fee275da066b60009e38963c
mbhadriraju <151967712+mbhadriraju@users.noreply.github.com>
2025-10-11 11:52:53 -0500

    Initial commit

---
+++ components/Input.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+  ({ className = "", type = "text", ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        ref={ref}
+        className={`
+          w-full
+          rounded-md
+          border
+          border-gray-300
+          bg-gray-300
+          px-4
+          py-2
+          text-base
+          text-gray-900
+          placeholder-gray-400
+          focus:outline-none
+          disabled:bg-gray-100
+          transition
+          font-subtitle
+          ${className}
+        `}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
+
+export default Input;
---
+++ components/VantaBackground.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import * as THREE from "three";
+import FOG from "vanta/dist/vanta.fog.min"; 
+
+export default function VantaBackground() {
+  const vantaRef = useRef<HTMLDivElement | null>(null);
+  const [vantaEffect, setVantaEffect] = useState<any>(null);
+
+  useEffect(() => {
+    if (!vantaEffect && vantaRef.current) {
+      setVantaEffect(
+        FOG({
+            el: vantaRef.current,
+            THREE,
+            highlightColor: 0xe19524,
+            midtoneColor: 0xc8898d,
+            lowlightColor: 0xdfdfdf,
+            baseColor: 0xffe0cd,
+            blurFactor: 0.50
+        })
+      );
+    }
+
+    return () => {
+      vantaEffect?.destroy();
+    };
+  }, [vantaEffect]);
+
+  return <div ref={vantaRef} className="fixed top-0 left-0 w-full h-full -z-10" />;
+}
